b88a93b57faaeae259bcf5a28d87bf5afcef205e
geraint <geraint.fisher@gmail.com>
2020-11-08 11:13:25 +0000

    update config to load only correct number of fonts & swith font display to block

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,12 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `Fredoka+One`,
-          `karla:300,400,500,600,700,800`,
-          `rubik:400,500,600,700,800`,
-        ],
-        display: "swap",
+        fonts: [`Fredoka+One`, `karla:400`, `rubik:600`],
+        display: "block",
       },
     },
   ],
