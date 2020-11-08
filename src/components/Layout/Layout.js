583644702e2a262097d08f7efc5b1cb4993b967b
geraint <geraint.fisher@gmail.com>
2020-11-08 14:55:11 +0000

    Preconnect to image and fonts domains

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,6 +7,9 @@ import GlobalStyles from "src/theme/GlobalStyles";
 const Layout = ({ children }) => (
   <Fragment>
     <Helmet>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://images.ctfassets.net" />
       <meta charSet="utf-8" />
       <title>Geraint Fisher</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
