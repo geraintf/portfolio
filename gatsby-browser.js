45d7b2a810eb7cfb8f0a23488a352a65769db847
geraint <geraint.fisher@gmail.com>
2020-11-08 02:02:50 +0000

    revert changes to ssr gatsby config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,6 @@
 const React = require("react");
 const Layout = require("./src/components/Layout/Layout").default;
 
-const wrapPageElement = ({ element, props }) => {
+exports.wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
-};
-
-module.exports = {
-  wrapPageElement,
 };
